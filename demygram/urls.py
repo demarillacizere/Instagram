7de763b255a7c727a603cef5c196e12bd06969f0
Demarillac Izere <izered3@gmail.com>
2020-12-29 14:31:29 +0200

    added the first view function

---
+++ demygram/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$',views.insta,name='insta')
+]
